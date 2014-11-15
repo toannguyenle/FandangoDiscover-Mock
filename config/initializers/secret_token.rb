12e7f25a4748d8437ebaa92cff0f1fc14b36b161
toannguyenle <toan@me.com>
2014-11-15 09:37:44 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Fandango::Application.config.secret_key_base = 'b82e6dfee243836ebc8dc18cf4eb6f5b9308803f47ad38b41df375de1cd31116113eb54b25c1e42f5a8fc8edd830ef7727cfc1c990be4c92f76568162e729abf'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+Fandango::Application.config.session_store :cookie_store, key: '_fandango_session'
